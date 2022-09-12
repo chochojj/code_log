28d9ca2cda1b3f0d362d21c0e607fa7bf0fe31f7
chochojj <csgy114@gmail.com>
2022-09-12 19:08:52 +0900

    first commit

---
+++ MyButton.js
@@ -0,0 +1,29 @@
+/*
+function MyButton (props) {
+    const [isClicked, setIsClicked] = React.useState(false); 
+    
+    
+    return React.createElement(
+        'button' ,
+        { onClick: () => setIsClicked(true) },
+        isClicked ? 'Clicked!' : 'Click here!'
+    )
+}
+
+const domContainer = document.querySelector('#root');
+ReactDom.render(React.createElement(MyButton),domContainer);
+*/
+
+
+function MyButton(props) {
+    const [isClicked, setIsClicked] = React.useState(false);
+
+    return React.createElement(
+        'button' ,
+        {onClick: () => setIsClicked(true) },
+        isClicked ? 'Clicked!' : 'Click here !' 
+    )
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement(MyButton), domContainer);
