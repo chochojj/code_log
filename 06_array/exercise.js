f8a017925e4724cf381decee7445f18af6052d53
chochojj <csgy114@gmail.com>
2022-12-29 22:27:52 +0900

    배열 연습문제
    주석 완

--- 06_array/exercise.js
+++ 06_array/exercise.js
@@ -224,108 +224,187 @@ function getElementsAfter(arr, n) {
 
 //15번 -- 배열과 인덱스를 입력받아 주어진 인덱스 이전의 요소들을 갖는 새로운 배열을 리턴
 function getElementsUpTo(arr, n) {
-if(arr.length < n || n === 0 || arr.length === 0){  //
-    return arr = [];    
-}
-return arr.slice(0,n);
-}
-
-//16번
-function getAllElementsButFirst(arr) {
-arr.shift()
-return arr;
-}
-
-//17번
-function getAllElementsButLast(arr) {
-arr.pop();
-return arr;
-}
-
-//18번
-function removeFromFront(arr) {
-arr.shift();
-return arr;
-}
-
-//19번 
-function removeFromBack(arr) {
-arr.pop();
-return arr;
-}
-
-//20번
-function removeFromBackOfNew(arr) {
-let str = arr.slice(0, arr.length-1);
-return str;
-}
-
-//21번  -- 배열에 문자열 넣기
-function addToBackOfNew(arr, el) {
-return arr.concat(el);
-}
-
-function addToBackOfNew(arr, el) {
-const newArr = arr.slice();
-newArr.push(el);
-return newArr;
-}
-
-//22번
-function addToFrontOfNew(arr, el) {
-let newArr = arr.slice(0);
-newArr.unshift(el);
-return newArr;
-}
-
-//23번
-function getAllElementsButNth(arr, n) {
-let result = [];
-for(let i = 0; i <arr.length; i++){
-    if(i !== n){
-    result.push(arr[i]);
+    if(arr.length < n || n === 0 || arr.length === 0){  //배열의 길이가 n보다 작거나, n이 0이거나, 빈 문자열일 때
+        return arr = [];                                //arr에 빈 문자열을 리턴
     }
+    return arr.slice(0,n);                              //arr배열을 0번 인덱스에서부터 n번 인덱스 앞까지 잘라서 리턴
 }
-return result;
+
+
+
+//16번 -- 배열을 입력받아 첫번째 요소를 제외한 배열을 리턴
+function getAllElementsButFirst(arr) {
+    arr.shift()                                         //shift 매서드로 맨 앞에 요소를 제거해줌
+    return arr;                                         //앞 요소를 제거해준 배열arr를 리턴
 }
 /*
-n전을 자르고 n뒤를 잘라 이어붙이는 방법도 있음
- front = arr.splice(0,3) 
- back = arr.slice(n+1)
- front.concat(back)
+function getAllElementsButFirst(arr) {              
+  return arr.slice(1);
+} 
+*/
+
+
+
+//17번 -- 배열을 입력받아 마지막 요소를 제외한 배열을 리턴
+function getAllElementsButLast(arr) {
+    arr.pop();                                          //pop 매서드로 맨 뒤의 요소를 제거
+    return arr;                                         //마지막 요소를 제거해준 배열arr를 리턴
+}
+/*
+function getAllElementsButLast(arr) {
+  return arr.slice(0, arr.length - 1);
+}
+*/
+
+
+
+//18번 -- 배열을 입력받아 배열의 첫번째 요소가 삭제된 배열을 리턴
+//!기존 배열에 첫번째 요소가 삭제된 배열(주소값 동일)을 리턴해야함
+function removeFromFront(arr) {
+    arr.shift();                                        //16번 문제와 비교 slice 매서드는 동일한 주소값(인스턴스)를 리턴하지 않음            
+    return arr;
+}
+/*
+//인스턴스 => 다음 챕터 객체 파트에서 알아보기!
+*/
+
+
+
+//19번 -- 배열을 입력받아 배열의 마지막 요소를 삭제한 배열을 리턴
+function removeFromBack(arr) {
+    arr.pop();                                         //17번 문제와 비교 - slice 매서드는 동일한 주소값(인스턴스)를 리턴하지 않음
+    return arr;
+}
+
+
+
+//20번 -- 배열을 입력받아 배열의 마지막 요소가 제외된 새로운 배열을 리턴
+function removeFromBackOfNew(arr) {             
+    let str = arr.slice(0, arr.length-1);               //arr배열 길이의 마지막 요소 앞까지 잘라서 변수 str에 할당
+    return str;                                         //변수 str리턴
+}
+
+
+
+//21번  -- 배열을 입력받아 새로운 요소가 추가된 새로운 배열을 리턴
+function addToBackOfNew(arr, el) {
+    return arr.concat(el);                              //arr배열에 push매서드로 el추가
+}
+/* 
+다른 방법
+function addToBackOfNew(arr, el) {
+    let newArray = arr.slice();                         //newArray변수에 slice로 한번 복사해서 할당
+    newArray.push(el);                                  //newArray변수의 뒤에 el 추가
+    return newArray;                                    //newArray를 리턴
+}
+*/
+
+
+
+//22번 -- 배열과 요소를 입력받아 맨앞에 새로운 요소가 추가된 새로운 배열을 리턴
+function addToFrontOfNew(arr, el) {
+    let newArr = arr.slice();                           //newArr변수에 slice로 한번 복사해서 할당
+    newArr.unshift(el);                                 //newArr변수의 앞에 el 추가
+    return newArr;                                      //newArr 리턴
+}
+
+
+
+//23번 -- 배열과 인덱스를 입력받아 해당 인덱스의 요소를 제외한 배열을 리턴
+function getAllElementsButNth(arr, n) {
+    let result = [];                                    //빈 배열을 result에 할당
+    for(let i = 0; i <arr.length; i++){                 //0부터 배열 길이까지 하나씩 증가시키며 검사하는데
+        if(i !== n){                                    //만약 i === n이 아니면 => n번째 인덱스 요소가 아니면
+        result.push(arr[i]);                            //빈 배열에 i번째 인덱스를 추가
+        }
+    }
+    return result;                                      // 0에서 i까지 n번째 인덱스 요소를 제외한 요소가 추가된 배열 리턴
+}
+/*
+// 1.n전까지 자르고    2. n뒤부터 잘라    3. 둘을 이어붙이는 방법도 있음
+ function getAllElementsButNth(arr, n) {
+  let front = arr.slice(0,n);               -- 1
+  let back = arr.slice(n+1);                -- 2
+  let sum = front.concat(back);             -- 3
+    
+  return sum;
+}
+
  */
 
-//24번
+
+
+//24번 -- 0~9 사이의 정수를 요소로 갖는 배열을 입력받아 전화번호 형식의 문자열로 리턴
+//예시 -- ['0','1','0','1','2','3','4','5','6','7','8'] => (010)1234-5678
 function createPhoneNumber(arr) {
 
-let firstNum = arr.slice(0,3).join('');
-let secondNum = arr.slice(arr.length-8, arr.length-4).join('');
-let lastNum = arr.slice(arr.length-4, arr.length).join('');
+    let firstNum = arr.slice(0,3).join('');                                     //앞의 배열 3개를 잘라내 공백을 기준으로 join으로 합쳐줌 - firstNum에 할당
+    let secondNum = arr.slice(arr.length-8, arr.length-4).join('');             //중간 4개의 배열 요소를 추출해 공백을 기준으로 join으로 합쳐줌 - secondNum에 할당
+    let lastNum = arr.slice(arr.length-4, arr.length).join('');                 //뒤의 4개의 배열 요소를 추출해 공백을 기준으로 join으로 합쳐줌 - lastNum에 할당
 
-if(arr.length === 8){
-    return `(010)${secondNum}-${lastNum}`;
-};
+    if(arr.length === 8){                                                       //010이 없는 뒷자리 숫자 8자리 배열만 입력받았을 경우 
+        return `(010)${secondNum}-${lastNum}`;                                  //010을 붙혀 출력
+    };
 
-return `(${firstNum})${secondNum}-${lastNum}`;
-
-
-
+    return `(${firstNum})${secondNum}-${lastNum}`;                              //템플릿 리터럴 firstNum,secondNum,lastNum 문자열로 다듬어 출력;
 }
+/*
+function createPhoneNumber(arr) {
+  // 경우의 수가 많아.....
+  // 입력: [0, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8], [8, 7, 6, 5, 4, 3, 2, 1])
+  // 출력: '(010)1234-5678', '(010)8765-4321'
+  // 11자리일 때도 있고, 8자리일 때도 있음.
+  // '(0번째 요소~ 2번째 요소)3번째 요소 ~ 6번째 요소-7번째요소 ~ 11번째요소'
+  // `(${0~2})${3~6}-${7~11}`
+  // 괄호 안에 들어가는 걸 first
+  // 하이픈 앞에 오는걸 second
+  // 하이픈 뒤에 오는 걸 third
+  // 원본 배열에서 복사한 다음, 복사한걸 문자열로 변환
 
-//25번
+  let first = arr.slice(0, 3).join(''); // [0, 1, 0] -> '010'
+  let second = arr.slice(-8, -4).join(''); // '1234'
+  let third = arr.slice(-4).join(''); // '5678'
+
+  if(arr.length === 8) {
+    first = '010';
+  }
+
+return `(${first})${second}-${third}`;
+}
+ */
+
+
+
+//25번 -- num를 입력받아 입력받은 num번째까지 총 num+1개의 피보나치 수열을 리턴
 function fibonacci(num) {
-let arr = [0,1];
-for(i = 0; i<= num; i++ ){
-    if(i === 0){
-    arr = [0]
-    }else if(i === 1){
-    arr = [0,1]
-    }else{
-    arr.push(arr[i-2]+arr[i-1]);
-    
+    let arr = [0,1];                        // arr변수에 이미 [0,1]배열을 할당
+    for(i = 0; i<= num; i++ ){              // 0부터 num번째까지 1씩 증가하며 반복
+        if(i === 0){                        // num이 0일때 
+        arr = [0] ;                         // 배열 arr 를 [0]으로 출력
+        }else if(i === 1){                  // num이 1일때 
+        arr = [0,1] ;                       // 배열 arr 를 [0,1]으로 출력
+        }else{
+        arr.push(arr[i-2]+arr[i-1]);        // 그렇지 않은 경우에 i-2인덱스 요소와 i-1인덱스 요소를 더해서 배열에 i번째 요소로 추가해줌
+        
+        }
     }
+    return arr;                             //num까지 반복하여 추가된 배열 arr를 리턴
 }
-return arr;
+/*
+//같은 코드를 다음과 같이 다듬을 수 있음
+function fibonacci(num) {
+  let arr = [0,1];                          // arr변수에 이미 [0,1]배열을 할당
+  if(num === 0){                            // num이 0일 경우 미리 걸러줌
+      arr = [0]
+  }
+  if(num === 1){                            // num이 1일 경우 미리 걸러줌
+      arr = [0,1];
+  }
+  for(i = 2; i<= num; i++ ){                // i를 2부터 시작해서 num까지 하나씩 증가하며 반복
+      arr.push(arr[i-2]+arr[i-1]);          // i-2인덱스 요소와 i-1인덱스 요소를 더해서 배열에 i번째 요소로 추가해줌
+  }
+  return arr;                               //num까지 반복하여 추가된 배열 arr를 리턴
 }
+*/
 
 
