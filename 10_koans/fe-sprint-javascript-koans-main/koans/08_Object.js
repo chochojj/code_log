002af30de3773d33094525ccf2774e2b5029b906
chochojj <csgy114@gmail.com>
2023-01-04 16:38:35 +0900

    koans 페어 활동
    완

--- 10_koans/fe-sprint-javascript-koans-main/koans/08_Object.js
+++ 10_koans/fe-sprint-javascript-koans-main/koans/08_Object.js
@@ -5,8 +5,9 @@ describe('Object에 대해서 학습합니다.', function () {
   */
   it('Object의 기본을 확인합니다.', function () {
     const emptyObj = {};
-    expect(typeof emptyObj === 'object').to.equal(FILL_ME_IN);
-    expect(emptyObj.length).to.equal(FILL_ME_IN);
+    expect(typeof emptyObj === 'object').to.equal(true);
+    expect(emptyObj.length).to.equal(undefined);
+    //빈 배열은 0, 빈 객체는 undefined
 
     const megalomaniac = {
       mastermind: 'Joker',
@@ -23,34 +24,42 @@ describe('Object에 대해서 학습합니다.', function () {
       },
     };
 
-    expect(megalomaniac.length).to.equal(FILL_ME_IN);
-    expect(megalomaniac.mastermind).to.equal(FILL_ME_IN);
-    expect(megalomaniac.henchwoman).to.equal(FILL_ME_IN);
-    expect(megalomaniac.henchWoman).to.equal(FILL_ME_IN);
-    expect(megalomaniac.getMembers()).to.deep.equal(FILL_ME_IN);
-    expect(megalomaniac.relations[FILL_ME_IN]).to.equal('Lucy');
-    expect(megalomaniac.twins['Heath Ledger']).to.deep.equal('FILL_ME_IN');
+    expect(megalomaniac.length).to.equal(undefined);
+    //length라는 속성값을 물어봄??
+    expect(megalomaniac.mastermind).to.equal('Joker');
+    expect(megalomaniac.henchwoman).to.equal('Harley');
+    expect(megalomaniac.henchWoman).to.equal(undefined);
+    expect(megalomaniac.getMembers()).to.deep.equal(['Joker','Harley']);
+    //함수실행 리턴값을 받아옴
+    expect(megalomaniac.relations[2]).to.equal('Lucy');
+    expect(megalomaniac.twins['Heath Ledger']).to.deep.equal('The Dark Knight');
   });
 
   it('Object의 속성(property)를 다루는 방법을 확인합니다.', function () {
     const megalomaniac = { mastermind: 'Agent Smith', henchman: 'Agent Smith' };
 
-    expect('mastermind' in megalomaniac).to.equal(FILL_ME_IN);
+    expect('mastermind' in megalomaniac).to.equal(true);
 
     megalomaniac.mastermind = 'Neo';
-    expect(megalomaniac['mastermind']).to.equal(FILL_ME_IN);
+    expect(megalomaniac['mastermind']).to.equal('Neo');
 
-    expect('secretary' in megalomaniac).to.equal(FILL_ME_IN);
+    expect('secretary' in megalomaniac).to.equal(false);
 
     megalomaniac.secretary = 'Agent Smith';
-    expect('secretary' in megalomaniac).to.equal(FILL_ME_IN);
+    expect('secretary' in megalomaniac).to.equal(true);
 
     delete megalomaniac.henchman;
-    expect('henchman' in megalomaniac).to.equal(FILL_ME_IN);
+    expect('henchman' in megalomaniac).to.equal(false);
   });
 
   it("'this'는 method를 호출하는 시점에 결정됩니다.", function () {
     const currentYear = new Date().getFullYear();
+    //<new Date() 예시> const date1 = new Date('December 17, 1995 03:24:00');
+    // Sun Dec 17 1995 03:24:00 GMT..
+    //<getfullYear() 예시> const moonLanding = new Date('July 20, 69 00:20:18');
+    //console.log(moonLanding.getFullYear());  --년도 값을 가져옴
+    // expected output: 1969
+
     const megalomaniac = {
       mastermind: 'James Wood',
       henchman: 'Adam West',
@@ -63,14 +72,15 @@ describe('Object에 대해서 학습합니다.', function () {
       },
     };
 
-    expect(currentYear).to.equal(FILL_ME_IN);
-    expect(megalomaniac.calculateAge(currentYear)).to.equal(FILL_ME_IN);
+    expect(currentYear).to.equal(2023);
+    expect(megalomaniac.calculateAge(currentYear)).to.equal(53);
 
     megalomaniac.birthYear = 2000;
-    expect(megalomaniac.calculateAge(currentYear)).to.equal(FILL_ME_IN);
+    expect(megalomaniac.calculateAge(currentYear)).to.equal(23);
 
     megalomaniac.changeBirthYear(2010);
-    expect(megalomaniac.calculateAge(currentYear)).to.equal(FILL_ME_IN);
+    expect(megalomaniac.calculateAge(currentYear)).to.equal(13);
+    //안쪽에서 birthYear에 대해 재할당을 해서 값을 바꿔줌
 
   /**
    * !!Advanced [this.mastermind]? this.birthYear? this가 무엇일까요?
@@ -104,17 +114,18 @@ describe('Object에 대해서 학습합니다.', function () {
       },
       battleCry(numOfBrains) {
         return `They are ${this.henchman} and the` + ` ${this.mastermind}`.repeat(numOfBrains);
+        //repeat는 반복해서 출력
       },
     };
 
-    expect(megalomaniac.getFusion()).to.deep.equal(FILL_ME_IN);
-    expect(megalomaniac.battleCry(3)).to.deep.equal(FILL_ME_IN);
+    expect(megalomaniac.getFusion()).to.deep.equal('PinkyBrain');
+    expect(megalomaniac.battleCry(3)).to.deep.equal(`They are Pinky and the Brain Brain Brain`);
   });
 
   it('Object를 함수의 전달인자로 전달할 경우, reference가 전달됩니다.', function () {
     const obj = {
-      mastermind: 'Joker',
-      henchwoman: 'Harley',
+      mastermind: 'Joker',    
+      henchwoman: 'Harley',   //139 줄 실행 'Adam West'  -- 152 줄 실행 'Harley'
       relations: ['Anarky', 'Duela Dent', 'Lucy'],
       twins: {
         'Jared Leto': 'Suicide Squad',
@@ -128,22 +139,40 @@ describe('Object에 대해서 학습합니다.', function () {
       refObj.henchwoman = 'Adam West';
     }
     passedByReference(obj);
-    expect(obj.henchwoman).to.equal(FILL_ME_IN);
+    expect(obj.henchwoman).to.equal('Adam West');
 
     const assignedObj = obj;
     assignedObj['relations'] = [1, 2, 3];
-    expect(obj['relations']).to.deep.equal(FILL_ME_IN);
+    expect(obj['relations']).to.deep.equal( [1, 2, 3]);
 
     const copiedObj = Object.assign({}, obj);
     copiedObj.mastermind = 'James Wood';
-    expect(obj.mastermind).to.equal(FILL_ME_IN);
+    expect(obj.mastermind).to.equal('Joker');
 
     obj.henchwoman = 'Harley';
-    expect(copiedObj.henchwoman).to.equal(FILL_ME_IN);
+    expect(copiedObj.henchwoman).to.equal('Adam West');
 
     delete obj.twins['Jared Leto'];
-    expect('Jared Leto' in copiedObj.twins).to.equal(FILL_ME_IN);
+    expect('Jared Leto' in copiedObj.twins).to.equal(false);
 
+    /*
+    const object = {
+      a: "a",
+      number: {
+        one: 1,
+        two: 2,
+      },
+    };
+    
+    const copy = Object.assign({}, object);
+    
+    copy.number.one = 3;
+    
+    console.log(object === copy); // false
+    console.log(object.number.one  === copy.number.one); // true
+    깊은 클로닝에 대해서, Object.assign() 은 속성의 값을 복사하기때문에 다른 대안을 사용해야합니다. 
+    출처 값이 객체에 대한 참조인 경우, 참조 값만을 복사합니다.
+    */
     /*
     마지막 테스트 코드의 결과가 예상과는 달랐을 수도 있습니다.
     'Object.assign'을 통한 복사는 reference variable은 주소만 복사하기 때문입니다. 
