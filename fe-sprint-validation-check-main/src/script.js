79209072787e01f62ad87630470dae13ba89cb60
chochojj <csgy114@gmail.com>
2023-01-05 23:46:58 +0900

    유효성 이벤트 업뎃

--- fe-sprint-validation-check-main/src/script.js
+++ fe-sprint-validation-check-main/src/script.js
@@ -6,12 +6,18 @@ const elInputPassword = document.querySelector('#password');
 const elInputPasswordCheck = document.querySelector('#password-retype');
 const elPasswordMiss = document.querySelector('.mismatch-message');
 const fillPassword = document.querySelector('.fillpassword-message');
-
-
+/* 
+//아이디/비밀번호 유효성 검사도 하고싶음! > 만약 한다면 각 이벤트 어디에 들어가야할지
+let checkNum = password.search(/[0-9]/g); // 숫자사용
+let checkEng = password.search(/[a-z]/ig); // 영문사용
+*/
 
 //아이디 입력창elInputUserName에 글자를 키보드로 입력할 떄.onkeyup
 elInputUserName.onkeyup = function (){
-  if(isMoreThan4Length(elInputUserName.value)){
+  if(elInputUserName.value === ''){             //아이디 인풋창에 값이 없을때
+    elfailuremessage.classList.add('hide');
+    elsuccessmessage.classList.add('hide');
+  }else if(isMoreThan4Length(elInputUserName.value)){
     //성공메세지
     elsuccessmessage.classList.remove('hide');
     //실패메세지 가려짐
@@ -23,17 +29,23 @@ elInputUserName.onkeyup = function (){
     elfailuremessage.classList.remove('hide');
   }
 }
+//if구문을 통해 아이디 인풋창에 값을 넣었다 지워도 남아있는 하단 메세지를 가려줌
+
 
 //비밀번호를 입력했을때
-//비밀번호 확인칸을 먼저 기입하고 비밀번호를 눌렀을때 fullpassword 메세지가 사라지지 않는 오류
+//비밀번호 확인칸을 먼저 기입하고 비밀번호를 눌렀을때 fillpassword 메세지가 사라지지 않는 오류
 elInputPassword.oninput = function(){
   //비밀번호 확인칸이 빈칸이 아니면
-  if(elInputPasswordCheck !== ''){
-    if(isMatch(elInputPassword.value, elInputPasswordCheck.value)){
-      elPasswordMiss.classList.add('hide');
-    }else{
-      fillPassword.classList.add('hide');
-      elPasswordMiss.classList.remove('hide');
+  if(elInputPasswordCheck.value !== '' && elInputPassword.value === ''){    //비밀번호 칸이 비고 비밀번호 확인값이 채워진 경우에
+    fillPassword.classList.remove('hide');                                  //비밀번호를 먼저 채워달라는 메세지 출력
+    elPasswordMiss.classList.add('hide');                                     
+  }
+  else if(elInputPasswordCheck !== ''){
+    if(isMatch(elInputPassword.value, elInputPasswordCheck.value)){  //확인칸이 빈칸이 아니고 비밀번호와 확인칸의 값이 같을때
+      elPasswordMiss.classList.add('hide');                           //두 값이 다르다는 메세지 지워줌
+    }else{                                          //확인칸이 빈칸이 아니지만 두 값이 다를때
+      fillPassword.classList.add('hide');             //칸을 채워달라는 메세지를 지우고
+      elPasswordMiss.classList.remove('hide');        //값이 다르다는 메세지를 출력
     }
   
   }
@@ -42,7 +54,7 @@ elInputPassword.oninput = function(){
 //비밀번호 확인칸을 기입했을 때
 elInputPasswordCheck.onkeyup = function (){
   if(elInputPasswordCheck.value === '' && elInputPassword.value === ''){    //비밀번호와 비밀번호 확인값이 모두 빈경우에
-    fillPassword.classList.add('hide');                                     //하단 메세지가 계속 남아있는 오류 수정
+    fillPassword.classList.add('hide');                                     
   }else if(elInputPassword.value === ''){                                   //비밀번호는 없는데 비밀번호 확인을 먼저 입력한 경우
     fillPassword.classList.remove('hide'); 
     elPasswordMiss.classList.add('hide');
@@ -56,13 +68,10 @@ elInputPasswordCheck.onkeyup = function (){
   }
 };
 
+//if문을 통해 비밀번호 값을 지운 이후 비밀번호 확인값을 지울때 하단 메세지가 계속 남아있는 오류 수정 
+//elInputPasswordCheck에 onchange 이벤트를 사용할 경우 마우스를 다른 영역 클릭해야 지워짐
+//onkeydown, onfocus, oninput 다 안됨 > 이미 사용되는 onkeyup 이벤트와 충돌해서 그런건지?
 
-//인풋창에 아무것도 없을 경우에
-//아이디와 비밀번호 확인창에 값을 기입했다 지웠을때  남아있는 메세지를 지움 > onchange 사용
-//인풋값을 모두 지우고 마우스로 다른 영역을 클릭하면 하단 메세지를 지움
-//onkeyup, onkeydown, onfocus, oninput 다 안됨
-//값이 사라지는 순간 메세지도 사라지게 하고 싶다...위에 이미 onkeyup을 사용해서 안되는건가?
-//onkeyup안에 병합하는 방법 시도하기
 
 
 
